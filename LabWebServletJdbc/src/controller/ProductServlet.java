405c4dc78121dfe7b9beb231346a6d861602fa28
Joe Tsai <giltwg@gmail.com>
2016-02-03 16:55:25 +0800

    my first commit! from Joe Tsai.
    hahaha
    remain you mind!

---
+++ LabWebServletJdbc/src/controller/ProductServlet.java
@@ -0,0 +1,141 @@
+package controller;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.ProductBean;
+import model.ProductService;
+
+@WebServlet(
+		urlPatterns={"/pages/product.controller"}
+)
+public class ProductServlet extends HttpServlet {
+	private static SimpleDateFormat sFormat = new SimpleDateFormat("yyyy-MM-dd");
+	private ProductService productService = new ProductService();
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+//接收資料
+		String temp1 = request.getParameter("id");
+		String name = request.getParameter("name");
+		String temp2 = request.getParameter("price");
+		String temp3 = request.getParameter("make");
+		String temp4 = request.getParameter("expire");
+		String prodaction = request.getParameter("prodaction");
+
+//轉換資料
+		Map<String, String> error = new HashMap<String, String>();
+		request.setAttribute("error", error);
+
+		int id = 0;
+		if(temp1!=null && temp1.length()!=0) {
+			try {
+				id = Integer.parseInt(temp1);
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+				error.put("id", "Id must be an integer");
+			}
+		}
+		double price = 0;
+		if(temp2!=null && temp2.length()!=0) {
+			try {
+				price = Double.parseDouble(temp2);
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+				error.put("price", "Price must be a number");
+			}
+		}
+		java.util.Date make = null;
+		if(temp3!=null && temp3.length()!=0) {
+			try {
+				make = sFormat.parse(temp3);
+			} catch (ParseException e) {
+				e.printStackTrace();
+				error.put("make", "Make must be a Date with yyyy-MM-dd");
+			}
+		}
+		int expire = 0;
+		if(temp4!=null && temp4.length()!=0) {
+			try {
+				expire = Integer.parseInt(temp4);
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+				error.put("expire", "Expire must be an integer");
+			}
+		}
+		
+//驗證資料
+		if("Insert".equals(prodaction) || "Update".equals(prodaction) || "Delete".equals(prodaction)) {
+			if(id==0) {
+				error.put("id", "Please enter Id for "+prodaction);
+			}
+		}
+		if(error!=null && !error.isEmpty()){
+			request.getRequestDispatcher(
+					"/pages/product.jsp").forward(request, response);
+			return;
+		}
+//呼叫model
+		ProductBean bean = new ProductBean();
+		bean.setId(id);
+		bean.setName(name);
+		bean.setPrice(price);
+		bean.setMake(make);
+		bean.setExpire(expire);
+		
+//根據model執行結果顯示view
+		if("Select".equals(prodaction)) {
+			List<ProductBean> result = productService.select(bean);
+			request.setAttribute("select", result);
+			request.getRequestDispatcher(
+					"/pages/display.jsp").forward(request, response);
+		} else if("Insert".equals(prodaction)) {
+			ProductBean result = productService.insert(bean);
+			if(result==null) {
+				error.put("action", "Insert failed");
+			} else {
+				request.setAttribute("insert", result);
+			}
+			request.getRequestDispatcher(
+					"/pages/product.jsp").forward(request, response);
+		} else if("Update".equals(prodaction)) {
+			ProductBean result = productService.update(bean);
+			if(result==null) {
+				error.put("action", "Update failed");
+			} else {
+				request.setAttribute("update", result);
+			}
+			request.getRequestDispatcher(
+					"/pages/product.jsp").forward(request, response);
+		} else if("Delete".equals(prodaction)) {
+			boolean result = productService.delete(bean);
+			if(result) {
+				request.setAttribute("delete", result);
+			} else {
+				error.put("action", "Delete failed");
+			}
+			request.getRequestDispatcher(
+					"/pages/product.jsp").forward(request, response);
+		} else {
+			error.put("action", "Unknown action: "+prodaction);
+			request.getRequestDispatcher(
+					"/pages/product.jsp").forward(request, response);
+		}
+	}
+	@Override
+	protected void doPost(HttpServletRequest req,
+			HttpServletResponse resp) throws ServletException, IOException {
+		this.doGet(req, resp);
+	}
+}
